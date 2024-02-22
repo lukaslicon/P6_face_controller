576797be203a72aa6ac5690e714a62f12f5a5f0f
lukaslicon <lukas@licons.com>
2024-02-21 18:38:49 -0800

    basic model

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -5,11 +5,22 @@ from tensorflow.keras.optimizers import RMSprop, Adam
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
-        # Your code goes here
-        # you have to initialize self.model to a keras model
-        pass
+        self.model = Sequential([
+            Rescaling(1./255, input_shape=input_shape),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(128, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+            layers.Dense(512, activation='relu'),
+            layers.Dense(categories_count, activation='softmax')
+        ])
     
     def _compile_model(self):
-        # Your code goes here
-        # you have to compile the keras model, similar to the example in the writeup
-        pass
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
